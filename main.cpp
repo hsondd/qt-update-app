09fac750b0fcedea8cd4661a1427d5ef3bcf4f1d
sondh <son@emotiv.com>
2026-01-14 14:01:47 +0700

    Add application version definition and update updater settings in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QCoreApplication>
 #include <QQmlApplicationEngine>
 #include "updater.h"
 
@@ -11,14 +12,15 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationVersion(QStringLiteral(APP_VERSION));
 
 #ifdef Q_OS_MAC
     auto updater = new updater::SparkleUpdater();
-    updater->setAutomaticDownload(true);
+    updater->setAutomaticDownload(false);
     Updater::setInstance(updater);
 #else
     auto updater = new updater::DefaultUpdater();
-    updater->setAutomaticDownload(true);
+    updater->setAutomaticDownload(false);
     updater->setManifestUrl(
         QUrl(QStringLiteral("https://github.com/hsondd/qt-update-app/releases/latest/download/appcast-win.xml")));
     Updater::setInstance(updater);
